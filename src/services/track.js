92bd294b2d67e322c7f7128202416a256705b9a8
Leonardo Huttner <leonardohuttner@Leonardos-Mac-Pro.local>
2021-09-22 13:23:24 -0300

    Ajuste envs

--- src/services/track.js
+++ src/services/track.js
@@ -1,8 +1,6 @@
 // // import api from '../plugins/axios'
 import axios from 'axios'
 const formato = 'json'
-// const token = 'f3594a068410eba222b114884dfab801f644172698f05ab52f49403c2bad67fd'
-// const user = 'leonardohuttner@gmail.com'
 
 
 const getData = (codigo) => {
