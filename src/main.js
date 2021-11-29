7444483d187316b6a441dec681bc1f6837fda00f
leonardohuttner <leonardohuttner@gmail.com>
2021-11-29 19:51:31 -0300

    teste 2 one signal

--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@ import './plugins/axios'
 import App from './App.vue'
 import './quasar'
 import router from './router'
+import OneSignalVue from 'onesignal-vue'
 
 require('dotenv').config()
-
 Vue.config.productionTip = false
+
+Vue.use(OneSignalVue)
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  beforeMount(){
+    this.$OneSignal.init({ appId: 'a0b7f69d-71e7-4925-9e38-c7af21cd74c8' });
+  }
 }).$mount('#app')
